46f34e2ddb094ed76f418a40f1100e2026a671ba
tortoise1992 <772084015@qq.com>
2019-11-21 09:04:41 +0800

    20191121

---
+++ src/pages/Home/QuKuai/index.js
@@ -0,0 +1,62 @@
+import React, { Component,Fragment } from 'react'
+import { Col,Statistic, Card,Icon } from 'antd'
+export default class Qukuai extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Col span={6}>
+                <Card>
+          <Statistic
+            title="订单总数"
+            value={141}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<Icon type="profile" />}
+            suffix="个"
+          />
+        </Card>
+
+                </Col>
+                <Col span={6}>
+                <Card>
+          <Statistic
+            title="销售总额"
+            value={141}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<Icon type="wallet" />}
+            suffix="元"
+          />
+        </Card>
+
+                </Col>
+                <Col span={6}>
+                <Card>
+          <Statistic
+            title="客户总数"
+            value={141}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<Icon type="gold" />}
+            suffix="个"
+          />
+        </Card>
+
+                </Col>
+                <Col span={6}>
+                <Card>
+          <Statistic
+            title="在线客户"
+            value={141}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<Icon type="user" />}
+            suffix="个"
+          />
+        </Card>
+
+                </Col>
+            </Fragment>
+        )
+    }
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
-
+import React, { Component,Fragment } from 'react'
+import { Row,Col } from 'antd'
+import QuKuai from './QuKuai'
 export default class Index extends Component {
     render() {
         return (
-            <div>
-                首页
-            </div>
+            <Fragment>
+                <Row gutter={15}>
+                    <QuKuai/>
+                </Row>
+                <Row>
+                    <Col span={12}>
+                    </Col>
+                    <Col span={12}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={8}>
+                    </Col>
+                    <Col span={16}>
+                    </Col>
+                </Row>
+            </Fragment>
         )
     }
 }
