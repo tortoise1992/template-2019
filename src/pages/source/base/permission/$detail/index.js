73c17ecce5f54f8aa29c77764d4f6d682031cc3f
tortoise1992 <772084015@qq.com>
2019-12-16 11:08:31 +0800

    1216

--- src/pages/source/base/permission/$detail/index.js
+++ src/pages/source/base/permission/$detail/index.js
@@ -4,13 +4,37 @@
  *   - ./src/routes/PrivateRoute.js 
  */
 import React, { Component } from 'react'
-
-export default class Detail extends Component {
+import withRouter from 'umi/withRouter'
+import { Radio,Card } from 'antd'
+class Detail extends Component {
+    state={
+        currentId:0
+    }
+    componentDidMount() {
+        if(this.props.match.params.detail){
+            this.setState({
+                currentId:this.props.match.params.detail
+            })
+        }
+    }
+    
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <div style={{textAlign:'center',marginBottom:10}}>
+                    <Radio.Group defaultValue="a" buttonStyle="solid">
+                        <Radio.Button value="a">用户信息</Radio.Button>
+                        <Radio.Button value="b">操作权限</Radio.Button>
+                        <Radio.Button value="c">数据权限</Radio.Button>
+                    </Radio.Group>
+                </div>
+                <Card>
+
+                </Card>
+            </React.Fragment>           
+            
         )
     }
 }
+
+export default withRouter(Detail)
