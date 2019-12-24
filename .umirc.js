a7468eb96a59d4cc5d4564c6869004118562c004
tortoise1992 <772084015@qq.com>
2019-12-24 10:13:09 +0800

    划分layout

--- .umirc.js
+++ .umirc.js
@@ -1,16 +1,9 @@
-
+let routeconfig =require('./routeconfig.json')
 // ref: https://umijs.org/config/
 export default {
   treeShaking: true,
-  routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
-  ],
+  disableCSSModules:true,
+  routes:routeconfig,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
