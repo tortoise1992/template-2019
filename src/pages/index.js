6744e7e7651bf7c113beec7d13883b1d95f61c88
tortoise1992 <772084015@qq.com>
2019-12-24 11:10:44 +0800

    2131

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 export default class Index extends Component {
-  componentDidMount() {
-    axios.post('/cms/login',{}).then(res=>{
-      console.log(res)
-    })
-  }
+  // componentDidMount() {
+  //   axios.post('/cms/login',{}).then(res=>{
+  //     console.log(res)
+  //   })
+  // }
   
   render() {
     return (
