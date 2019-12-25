0d7f93c3fb6e2f82f5dd63d297322a8abb0c94bd
tortoise1992 <772084015@qq.com>
2019-12-25 11:26:37 +0800

    Update index.js

--- src/pages/Lanmu/index.js
+++ src/pages/Lanmu/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import { Card, Button, Menu, Icon, Dropdown } from 'antd'
+import withRouter from 'umi/withRouter'
+class Lanmu extends Component {
+    handleMenuClick = () => {
 
-export default class Lanmu extends Component {
+    }
     render() {
+        let menu = (
+            <Menu onClick={this.handleMenuClick}>
+                <Menu.Item key="1">
+                    增加顶级栏目
+                </Menu.Item>
+                <Menu.Item key="2">
+                    批量增加栏目
+                </Menu.Item>
+                
+            </Menu>
+        )
         return (
-            <div>
+            <Card title='栏目列表'
+                extra={
+                    <Dropdown overlay={menu}>
+                        <Button>
+                            栏目管理 <Icon type="down" />
+                        </Button>
+                    </Dropdown>
+                }>
                 栏目
-            </div>
+            </Card>
         )
     }
 }
+
+export default withRouter(Lanmu)
